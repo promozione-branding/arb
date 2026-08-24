cf03511f6b4f1bd91ec0ca82eb351ea112007274
manish kumar <mk7219630@gmail.com>
2026-08-24 14:26:15 +0530

    clarity code again

--- app/layout.js
+++ app/layout.js
@@ -65,28 +65,48 @@ export default function RootLayout({ children }) {
           }}
         />
 
-        <Script id="microsoft-clarity" strategy="beforeInteractive">
+       <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+        >
           {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){
-                    (c[a].q=c[a].q||[]).push(arguments)
-                };
-                t=l.createElement(r);
-                t.async=1;
-                t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];
-                y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "y783t114gh");
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TDPTJ48F');
           `}
         </Script>
       </head>
       <body className="min-h-full flex flex-col">
+
+         <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TDPTJ48F"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
         <Navbar categories={categories} />
         {children}
         <ToastContainer position="top-right" />
         <Footer />
         {/* Yandex.Metrika fallback for users without JavaScript */}
-        <noscript>
+        {/* <noscript>
           <div>
             <img
               src="https://mc.yandex.ru/watch/111678511"
@@ -97,7 +117,7 @@ export default function RootLayout({ children }) {
               alt=""
             />
           </div>
-        </noscript>
+        </noscript> */}
       </body>
     </html>
   );
